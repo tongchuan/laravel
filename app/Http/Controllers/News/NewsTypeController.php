df4428bcaf2a1798fe798e942e91fa97057d307d
tc <zhangtch>
2019-01-08 09:57:17 +0800

    update

--- app/Http/Controllers/News/NewsTypeController.php
+++ app/Http/Controllers/News/NewsTypeController.php
@@ -19,42 +19,42 @@ class NewsTypeController extends Controller
   public function add(Request $request){
 
     $data = array();
-    $data["parentid"]=$request->input("parentid",null);
+    // $data["parentid"]=$request->input("parentid",null);
     $data["name"]=$request->input("name",null);
-    $data["cnname"]=$request->input("cnname",null);
-    $data["enname"]=$request->input("enname",null);
-    $data["link_nav"]=$request->input("link_nav",null);
-    $data["content_nav"]=$request->input("content_nav",null);
-    $data["tags"]=$request->input("tags",null);
-    $data["tags_seo"]=$request->input("tags_seo",null);
-    $data["keyword"]=$request->input("keyword",null);
-    $data["remark"]=$request->input("remark",null);
-    $data["small_pic"]=$request->input("small_pic",null);
-    $data["big_pic"]=$request->input("big_pic",null);
-    $data["link"]=$request->input("link",null);
-    $data["link_spic"]=$request->input("link_spic",null);
-    $data["link_bpic"]=$request->input("link_bpic",null);
-    $data["show"]=$request->input("show",null);
-    $data["status"]=$request->input("status",null);
-    $data["hot"]=$request->input("hot",null);
-    $data["top"]=$request->input("top",null);
-    $data["recommend"]=$request->input("recommend",null);
-    $data["comm"]=$request->input("comm",null);
-    $data["vote"]=$request->input("vote",null);
-    $data["delete"]=$request->input("delete",null);
-    $data["create_man"]=$request->input("create_man",null);
-    $data["create_time"]=$request->input("create_time",null);
-    $data["modify_man"]=$request->input("modify_man",null);
-    $data["modify_time"]=$request->input("modify_time",null);
+    // $data["cnname"]=$request->input("cnname",null);
+    // $data["enname"]=$request->input("enname",null);
+    // $data["link_nav"]=$request->input("link_nav",null);
+    // $data["content_nav"]=$request->input("content_nav",null);
+    // $data["tags"]=$request->input("tags",null);
+    // $data["tags_seo"]=$request->input("tags_seo",null);
+    // $data["keyword"]=$request->input("keyword",null);
+    // $data["remark"]=$request->input("remark",null);
+    // $data["small_pic"]=$request->input("small_pic",null);
+    // $data["big_pic"]=$request->input("big_pic",null);
+    // $data["link"]=$request->input("link",null);
+    // $data["link_spic"]=$request->input("link_spic",null);
+    // $data["link_bpic"]=$request->input("link_bpic",null);
+    // $data["show"]=$request->input("show",null);
+    // $data["status"]=$request->input("status",null);
+    // $data["hot"]=$request->input("hot",null);
+    // $data["top"]=$request->input("top",null);
+    // $data["recommend"]=$request->input("recommend",null);
+    // $data["comm"]=$request->input("comm",null);
+    // $data["vote"]=$request->input("vote",null);
+    // $data["delete"]=$request->input("delete",null);
+    // $data["create_man"]=$request->input("create_man",null);
+    // $data["create_time"]=$request->input("create_time",null);
+    // $data["modify_man"]=$request->input("modify_man",null);
+    // $data["modify_time"]=$request->input("modify_time",null);
 // DB::table('users')->insert(
 //     ['email' => 'john@example.com', 'votes' => 0]
 // );
-    $data= json_decode(json_encode($data), ture);
+    // $data= json_decode(json_encode($data), ture);
     $id = DB::table($this->table)->insertGetId(
         $data
     );
     $data['id']=$id;
-    return json_encode(json_decode($data,true));
+    return json_encode($data);
     // $sql = "insert into ".$this->table."(`parentid`,`name`,`cnname`,`enname`,`link_nav`,`content_nav`,`tags`,`tags_seo`,`keyword`,`remark`,`small_pic`,`big_pic`,`link`,`link_spic`,`link_bpic`,`show`,`status`,`hot`,`top`,`recommend`,`comm`,`vote`,`delete`,`create_man`,`create_time`,`modify_man`,`modify_time`)"
     // $data = null;
     // $data['input'] = $request->input();
