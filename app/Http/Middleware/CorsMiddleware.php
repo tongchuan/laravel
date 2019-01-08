7ddfab5b1fe5f2fdc0a8ec6806c57a1699b5a0e9
tc <zhangtch>
2019-01-08 16:28:59 +0800

    update

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -17,7 +17,7 @@ class CorsMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
+        $response->header('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'false');
         return $response;
